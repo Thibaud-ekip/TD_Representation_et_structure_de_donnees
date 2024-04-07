fb44cb8d87d2669d9e461241239ddab2d880a573
Thibaud-ekip <163284746+Thibaud-ekip@users.noreply.github.com>
2024-04-07 15:40:33 +0200

    Add files via upload

--- TD 4.py
+++ TD 4.py
@@ -15,7 +15,6 @@ class Hashtable:
             if couple[0] == key:
                 couple[1] = value
         self.__table[index] = [(key,value)]+self.__table[index]
-        self.resize()
 
     def get(self,key):
         index = self.__hash(key)%self.__length
@@ -24,25 +23,29 @@ class Hashtable:
                 return couple[1]
         return None
 
-    def repartition(self):
+    def repartition(self,n):
         "graph of number of couple per value of hashf"
         X = []
         Y = []
         for i in range (self.__length):
             X.append(i)
             Y.append(len(self.__table[i]))
-        width = 1/1.5
+        width = 1/n
         plt.bar(X, Y, width, color="blue")
         plt.show()
 
     def resize(self):
+        "resize the hashtable"
+        h = Hashtable(self.__hash,2*self.__length)
+        for box in self.__table:
+            for couple in box:
+                h.put(couple[0],couple[1])
+        self.__table = h.__table
+        self.__length = 2*self.__length
+
+    def sizeup(self):
         if sum([len(box) for box in self.__table])>1.2*self.__length:
-            h = Hashtable(self.__hash,2*self.__length)
-            for box in self.__table:
-                for couple in box:
-                    h.put(couple[0],couple[1])
-            self.__table = h.__table
-            self.__length = 2*self.__length
+            self.resize()
 
 
 def h(str):
@@ -60,7 +63,7 @@ if __name__ == '__main__':
     H.put('a',1)
     H.put('b',2)
     assert(H.get('a')==1)
-    H.repartition()
+    H.repartition(1.5)
 
 
     words=[]
@@ -72,12 +75,12 @@ if __name__ == '__main__':
     L = Hashtable(h,320)      #table with 320 entrys
     for word in words:
         L.put(word,len(word))
-    L.repartition()
+    L.repartition(1)
 
     K = Hashtable(h,10000)    #table with 10000 entrys
     for word in words:
         K.put(word,len(word))
-    K.repartition()
+    K.repartition(1)
 
 
 
